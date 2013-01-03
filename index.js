aca90e29562f1f1aadc73d3e84f9df5132e7afed
mluto <m@luto.at>
2013-01-03 12:34:53 +0100

    Fixed template-naming, added icons

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_exportColumn = function(hook, context)
 {
-  var osfExports = eejs.require("ep_osfExport/templates/osfExports.html");
+  var osfExports = eejs.require("ep_osfExport/templates/osfExports.ejs");
   context.content = context.content + osfExports;
+}
+
+exports.eejsBlock_styles = function (hook, context)
+{
+  context.content = context.content + eejs.require("ep_osfExport/templates/styles.ejs");
 }
