bb6e1e5f8d372dbb3626065041d492b5d1a466ef
mluto <m@luto.at>
2013-01-03 10:24:05 +0100

    Added TODO, formatting

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_exportColumn = function(hook, context)
 {
-	var osfExports = eejs.require("ep_osfExport/templates/osfExports.html");
-	context.content = context.content + osfExports;
+  var osfExports = eejs.require("ep_osfExport/templates/osfExports.html");
+  context.content = context.content + osfExports;
 }
